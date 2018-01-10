f1a937969e5a06da0f03f5f52b131409f6bd592a
marigalicer <marigalicer@gmail.com>
2018-01-10 13:59:46 -0500

    add scroll and slider customization

--- js/init.js
+++ js/init.js
@@ -3,6 +3,14 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.scrollspy').scrollSpy({
+      scrollOffset: 50
+    });
+    $('.slider').slider({
+      height: 500,
+      indicators: false,
+      interval: 4000
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
